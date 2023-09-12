6c7835ef4ed6bae4336ceb7058f62677ed7e411a
Santiago Diaz <sddiaz@shockers.wichita.edu>
2023-09-11 20:11:44 -0500

    Merge Sort Working!

---
+++ Vizme (Algorithms and Data Structures Vizualizer)/vizme/src/components/Sorting/Algorithms/SortingAlgorithms.js
@@ -0,0 +1,65 @@
+  export function getMergeSortAnimations(array) {
+    const animations = [];
+    const helperArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, helperArray, animations);
+    return animations;
+  }
+  
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    helperArray,
+    animations,
+  ) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(helperArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(helperArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, helperArray, animations);
+  }
+  
+  function doMerge(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while (i <= middleIdx && j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, j]);
+      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+        // We overwrite the value at index k in the original array with the
+        // value at index i in the auxiliary array.
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+      } else {
+        // We overwrite the value at index k in the original array with the
+        // value at index j in the auxiliary array.
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+      }
+    }
+    while (i <= middleIdx) {
+      animations.push([i, i]);
+      animations.push([i, i]);
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
+    }
+    while (j <= endIdx) {
+      animations.push([j, j]);
+      animations.push([j, j]);
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+
--- Vizme (Algorithms and Data Structures Vizualizer)/vizme/src/components/Sorting/Sorting.js
+++ Vizme (Algorithms and Data Structures Vizualizer)/vizme/src/components/Sorting/Sorting.js
@@ -1,30 +1,117 @@
-import React, { useState } from "react";
-import Visualizer from "./SortingComponents/Visualizer";
+import React, { useState, useEffect } from "react";
 import { Button, Slider } from "@mui/material";
 import { styled, useTheme } from '@mui/material/styles';
+import SortingAlgorithms, { getMergeSortAnimations } from "./Algorithms/SortingAlgorithms";
 
 function Sorting() {    
 
     //#region Variables
 
-    const [duration, setDuration] = useState(100);
+    const [arr, setArr] = useState([]);
+    const [arrSize, setArrSize] = useState(10);
+    const [width, setWidth] = useState(0);
+    const [speed, setSpeed] = useState(100);
     const [resetKey, setResetKey] = useState(0.0);
     const [sliderPosition, setSliderPosition] = useState(10);
     const theme = useTheme();
-
-    const resetArray = () => {
+    // Reset array after Slider Changes
+    const changeKey = () => {
         setResetKey((prevKey) => prevKey + .1);
     }
 
     //#endregion
 
+    //#region Methods
+
+    window.addEventListener("resize", handleResize);
+    // Reset Array on Component Mount
+    useEffect(() => {
+      resetArray();
+    }, []);
+    // Handle State Changes on Slider Change
+    useEffect(() => {
+        handleResize(); // Update width when arrSize changes
+        setSpeed(1000/arrSize);
+        resetArray();
+    }, [arrSize]);
+    useEffect(() => {
+        resetArray();
+    }, [resetKey])
+    useEffect(() => {
+      setArrSize(sliderPosition);
+    }, [sliderPosition])
+
+    function resetArray() {
+        const newArr = [];
+        for (let i = 0; i < arrSize; i++) {
+            // We use window.innerHeight/2 as the maximum height to keep bars from overflowing into our UI.
+            newArr.push(randFromInterval(5, window.innerHeight / 2));
+        }
+        setArr(newArr);
+        resetStyling();
+    }
+
+    function handleResize() {
+        const newWidth = window.innerWidth < 1024 ? 
+            (window.innerWidth * 0.85) / arrSize :
+            (window.innerWidth * 0.9 * 0.75) / arrSize;
+        setWidth(newWidth);
+    }
+
+    function mergeSort(arr) {
+        const animations = getMergeSortAnimations(arr);
+        handleAnimations(animations);
+    }
+    // Function to change our bars based on the animations provided. 
+    function handleAnimations(animations) {
+      for (let i = 0; i < animations.length; i++) {
+        const arrayBars = document.getElementsByClassName('visualizer--bar');
+        const isColorChange = i % 3 !== 2;
+        if (isColorChange) {
+          const [barOneIdx, barTwoIdx] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          const barTwoStyle = arrayBars[barTwoIdx].style;
+          const color = i % 3 == 0 ? window.matchMedia('(prefers-color-scheme: dark)').matches ? '#800020' : '#000' : "rgba(0, 0, 0, 1)";
+          // Finished Elements.
+          setTimeout(() => {
+            barOneStyle.backgroundColor = color;
+            barTwoStyle.backgroundColor = color;
+          }, i * speed);
+        } else {
+          setTimeout(() => {
+            const [barOneIndex, newHeight] = animations[i];
+            const barOneStyle = arrayBars[barOneIndex].style;
+            barOneStyle.height = `${newHeight}px`;
+          }, i * speed);
+        }
+      }
+      // Reset Styling 
+      resetStyling(); 
+    }
+
+    function randFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    } 
+
+    function resetStyling() {
+      const arrayBars = document.getElementsByClassName('visualizer--bar');
+      const defaultStyling = window.matchMedia('(prefers-color-scheme: dark)').matches ? '#fff' : '#000';
+      for (let i = 0; i < arrayBars.length; i++) {
+          arrayBars[i].style.backgroundColor = defaultStyling; // Set to default color
+      }
+    }
+
+
+    //#endregion
+
+    //#region Component
 
     return (
         <div className="sorting">
             <div className="sorting--container">
                 <div className="sorting--description">
                         <div className="form--controls">
-                            <Button className="form--generateBtn" variant="contained" onClick={resetArray}>New Array</Button>
+                            <Button className="form--generateBtn" variant="contained" onClick={changeKey}>New Array</Button>
                             <Slider 
                                 defaultValue={10}
                                 aria-label="time-indicator"
@@ -62,18 +149,35 @@ function Sorting() {
                                     },
                                 }}
                             />
-                            {sliderPosition}
+                            <Button className="form--generateBtn" variant="contained" onClick={() => mergeSort(arr)}>Merge</Button>
                         </div>
                         <div className="form--info">
 
                         </div>
                     </div>
                 <div className="sorting--box">
-                    <Visualizer length={sliderPosition} resetKey={resetKey}/>
+                     <div className="visualizer--container">
+                      <div className="visualizer--bars">
+                        {arr.map((value, index) => (
+                          <div className="bar--stack">
+                            <div
+                              className="visualizer--bar"
+                              key={index}
+                              style={{
+                                height: `${value}px`,
+                                width: `${width}px`,
+                              }}
+                            ></div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
+
+    //#endregion
 }
 
 export default Sorting;
