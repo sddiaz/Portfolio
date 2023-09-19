fbee82a933850fd5c428d694f771a64587dfc63a
Santiago Diaz <sddiaz@shockers.wichita.edu>
2023-09-18 21:04:19 -0500

    Pseudocode + Improved Styling

--- Vizme (Algorithms and Data Structures Vizualizer)/vizme/src/components/Sorting/Algorithms/Description.js
+++ Vizme (Algorithms and Data Structures Vizualizer)/vizme/src/components/Sorting/Algorithms/Description.js
@@ -1,7 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { CopyBlock, dracula} from "react-code-blocks";
 
 function Description({page, info, menuValue}) {
 
+    const pseudocodeStlye = {
+        maxWidth: '150%',
+        fontSize: '15px',
+        overflowY: 'none',
+        overflowX: 'scroll',
+    }
+    const codeBlockStyle = {
+        marginBottom: '10px',
+    }
     return (
         // This class logic allows us to toggle 
         // varying pages via props.
@@ -9,13 +19,44 @@ function Description({page, info, menuValue}) {
             <div className={menuValue == 'Description' ? 'description--main' : 'hidden'}>
                 {info.Description}
             </div>
-            <div className={menuValue == 'Pseudocode' ? 'description--main' : 'hidden'}>
-                {info.Pseudocode}
+           {info.Pseudocode && 
+            <div className={menuValue == 'Pseudocode' ? 'description--pseudocode' : 'hidden'} style={{padding: '10px'}}> 
+                    <div style={codeBlockStyle}>
+                        JavaScript
+                        <CopyBlock 
+                        theme={dracula} 
+                        language={'javascript'} 
+                        text={info.Pseudocode.JavaScript}
+                        customStyle={pseudocodeStlye} 
+                        showLineNumbers
+                        />
+                    </div>
+                    <div style={codeBlockStyle}>
+                        C++
+                        <CopyBlock 
+                        theme={dracula} 
+                        language={'cpp'} 
+                        text={info.Pseudocode.CPP}
+                        customStyle={pseudocodeStlye} 
+                        showLineNumbers
+                        wrapLongLines/>
+                    </div>
+                    <div style={codeBlockStyle}>
+                        Python
+                        <CopyBlock 
+                        theme={dracula} 
+                        language={'python'} 
+                        text={info.Pseudocode.Python}
+                        customStyle={pseudocodeStlye} 
+                        showLineNumbers
+                        wrapLongLines/>
+                    </div> 
             </div>
-            <div className={menuValue == 'Performance' ? 'description--main' : 'hidden'}>
+            }
+            <div className={menuValue == 'Performance' ? 'description--performance' : 'hidden'}>
                 {info.Performance}
             </div>
-            <div className={menuValue == 'FurtherLearning' ? 'description--main' : 'hidden'}>
+            <div className={menuValue == 'FurtherLearning' ? 'description--furtherLearning' : 'hidden'}>
                 {info.FurtherLearning}
             </div>
         </div>
